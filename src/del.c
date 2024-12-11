64c2b0224b3f2224aeb2eb4fead9a7ec2109fde7
Inkurey22 <inkurey22.tr@proton.me>
2024-12-11 15:34:45 +0100

    feat: enhanced error handling

--- src/del.c
+++ src/del.c
@@ -9,6 +9,7 @@
 
 #include "my_lists.h"
 #include "my_printf.h"
+#include "my_strings.h"
 #include "my_numbers.h"
 
 #include "shell.h"
@@ -24,38 +25,54 @@ void free_device(void *data)
     free(device);
 }
 
-void del_node(list_t **list, int id)
+void delete_first_node(list_t **list, list_t *node, int *status)
+{
+    *list = node->next;
+    free_device(node->data);
+    free(node);
+    *status = 1;
+}
+
+void delete_middle_node(list_t *prev, list_t *node, int *status)
+{
+    prev->next = node->next;
+    free_device(node->data);
+    free(node);
+    *status = 1;
+}
+
+void del_node(list_t **list, int id, int *status)
 {
     list_t *tmp = *list;
     list_t *prev = NULL;
 
+    *status = 0;
     if (tmp == NULL)
         return;
     if (((device_t *)tmp->data)->id == id) {
-        *list = tmp->next;
-        free_device(tmp->data);
-        free(tmp);
+        delete_first_node(list, tmp, status);
         return;
     }
     while (tmp && ((device_t *)tmp->data)->id != id) {
         prev = tmp;
         tmp = tmp->next;
     }
-    if (tmp == NULL)
-        return;
-    prev->next = tmp->next;
-    free_device(tmp->data);
-    free(tmp);
+    if (tmp != NULL)
+        delete_middle_node(prev, tmp, status);
 }
 
 int del(void *data, char **args)
 {
     int ref = 0;
+    int status = 0;
 
     for (int i = 0; args[i] != NULL; i++) {
-        if (args[i] == NULL)
+        if (args[i] == NULL || my_str_isnum(args[i]) == 0)
             return 84;
         ref = my_getnbr(args[i]);
-        del_node(data, ref);
+        del_node(data, ref, &status);
+        if (status == 0)
+            return 84;
     }
+    return 0;
 }
